d853eb3ee3379dbd015a61e890c0773788d9132b
Ben K <benkornmeier@gmail.com>
2020-06-21 22:53:20 -0600

    clean up reassembly

--- src/flow/layer3/defrag/reassemble.rs
+++ src/flow/layer3/defrag/reassemble.rs
@@ -1,18 +1,16 @@
-// use crate::layer3::IPv4;
-// use crate::flow::layer3::ipv4::errors::{Error as Ipv4Error};
-//
-// pub fn ipv4<'a>(buffer: Vec<IPv4<'a>>) -> Result<IPv4, Ipv4Error> {
-//     let mut buffer = buffer;
-//     if buffer.is_empty() {
-//         return Err(Ipv4Error::Defrag{msg: String::from("empty buffer")})
-//     }
-//     let mut first = buffer.remove(0);
-//     let mut payload = Vec::new();
-//     payload.extend_from_slice(first.payload);
-//
-//     for packet in buffer.into_iter() {
-//         payload.extend_from_slice(packet.payload);
-//     }
-//     std::mem::replace(&mut first.payload, payload.as_slice());
-//     Ok(first)
-//}
+use crate::layer3::IPv4;
+use crate::layer3::ipv4::Payload;
+use crate::flow::layer3::ipv4::errors::{Error as Ipv4Error};
+
+pub fn ipv4<'a>(first: IPv4<'a>, buffer: Vec<IPv4<'a>>) -> Result<IPv4<'a>, Ipv4Error> {
+    let mut buffer = buffer;
+    let mut first = first;
+    let mut payload = Vec::new();
+    payload.extend_from_slice(&first.payload);
+
+    for packet in buffer.into_iter() {
+        payload.extend_from_slice(&packet.payload);
+    }
+    first.payload = Payload::Owned(payload);
+    Ok(first)
+}
